3cef97c5a294ad3621c5b5b8aa4b921b6c409307
gorengbebek237 <gorengbebek237@gmail.com>
2026-07-26 21:11:51 +0700

    Update Agro.ino

--- LSP_Kinan/Agrotech/Agro.ino
+++ LSP_Kinan/Agrotech/Agro.ino
@@ -1,135 +1,76 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <DHT.h>
-#include <WiFi.h>
-#include <ThingSpeak.h>
-#include <LittleFS.h>       
-#include <WebServer.h>      
 
-#define DHTPIN 18          
-#define DHTTYPE DHT21      
-#define SOIL_PIN 34        
-#define RELAY_PUMP 16      
-#define RELAY_FAN 27       
+#define SOIL_PIN 34
+#define RELAY_PUMP 16
 
-const char* ssid = "Man";                   
-const char* password = "Mantul123";         
-unsigned long myChannelNumber = 3400718;    
-const char * myWriteAPIKey = "36VRJGCPF1O6AHD5"; 
-
-WiFiClient client;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-DHT dht(DHTPIN, DHTTYPE);
 
-WebServer server(80);
-
-const int SOIL_DRY = 40;       
-const float TEMP_HOT = 25.0;   
-
-unsigned long lastTimeCloud = 0;
-const unsigned long timerDelayCloud = 20000; 
+const int SOIL_DRY = 40;
 
 unsigned long lastTimeSensor = 0;
-const unsigned long timerDelaySensor = 2000; 
+const unsigned long timerDelaySensor = 2000;
 
-float lastTemp = 0.0;
-float lastHum = 0.0;
 int lastSoil = 0;
 bool pumpState = false;
-bool fanState = false;
 
 void setup() {
   Serial.begin(115200);
 
+  Wire.begin(21, 22);
+
   lcd.init();
   lcd.backlight();
-  lcd.print("Memulai Sistem...");
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("Menghubungkan ke WiFi");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nTerhubung ke WiFi!");
-  Serial.print("IP Address Web Server: ");
-  Serial.println(WiFi.localIP()); 
-
-  if (!LittleFS.begin()) {
-    Serial.println("Gagal me-mount LittleFS!");
-    return;
-  }
-  Serial.println("LittleFS berhasil di-mount.");
-
-  server.on("/", HTTP_GET, []() {
-    File file = LittleFS.open("/index.html", "r");
-    if (!file) {
-      server.send(404, "text/plain", "Error: File index.html tidak ditemukan! Kamu belum melakukan ESP32 Sketch Data Upload.");
-      return;
-    }
-    server.streamFile(file, "text/html");
-    file.close();
-  });
-
-  server.serveStatic("/", LittleFS, "/");  
-
-  server.begin();
-  Serial.println("Web Server Aktif!");
-
-  ThingSpeak.begin(client); 
-  dht.begin();
-  pinMode(RELAY_PUMP, OUTPUT);
-  pinMode(RELAY_FAN, OUTPUT);
-  digitalWrite(RELAY_PUMP, LOW);
-  digitalWrite(RELAY_FAN, LOW);
-  
   lcd.clear();
-  lcd.print("Sistem Agrotek Siap!");
+  lcd.setCursor(0,0);
+  lcd.print("Memulai Sistem...");
+
+  pinMode(RELAY_PUMP, OUTPUT);
+  digitalWrite(RELAY_PUMP, LOW);
+
   delay(2000);
   lcd.clear();
+  Serial.println("Sistem Berjalan");
 }
 
 void loop() {
-  server.handleClient(); 
-
+  // Membaca Sensor dan Mengatur Pompa (Setiap 2 detik)
   if (millis() - lastTimeSensor >= timerDelaySensor) {
     lastTimeSensor = millis();
 
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
-    int soilAnalogValue = analogRead(SOIL_PIN);
+    int soilAnalog = analogRead(SOIL_PIN);
     
-    if (!isnan(humidity) && !isnan(temperature)) {
-      lastHum = humidity;
-      lastTemp = temperature;
-      int soilPercent = map(soilAnalogValue, 4095, 1000, 0, 100);
-      lastSoil = constrain(soilPercent, 0, 100);
+    // Mapping nilai analog ke persen (0-100%)
+    lastSoil = map(soilAnalog, 4095, 1000, 0, 100);
+    lastSoil = constrain(lastSoil, 0, 100);
 
-      pumpState = (lastSoil < SOIL_DRY);
-      digitalWrite(RELAY_PUMP, pumpState ? HIGH : LOW);
+    // --- LOGIKA KONTROL POMPA ---
+    pumpState = (lastSoil < SOIL_DRY);
+    digitalWrite(RELAY_PUMP, pumpState ? HIGH : LOW);
 
-      fanState = (lastTemp > TEMP_HOT);
-      digitalWrite(RELAY_FAN, fanState ? HIGH : LOW);
+    // --- TAMPILAN LCD ---
+    lcd.setCursor(0,0);
+    lcd.print("Kelembapan Tanah");
 
-      lcd.setCursor(0, 0); lcd.print("T:" + String(lastTemp, 1) + "C H:" + String(lastHum, 1) + "%  ");
-      lcd.setCursor(0, 1); lcd.print("Soil: " + String(lastSoil) + "%   ");
-      lcd.setCursor(0, 2); lcd.print("PUMP:" + String(pumpState ? "ON " : "OFF") + " FAN:" + String(fanState ? "ON " : "OFF"));
-    }
-  }
+    lcd.setCursor(0,1);
+    lcd.print("Nilai : ");
+    lcd.print(lastSoil);
+    lcd.print("%    ");
 
-  if (millis() - lastTimeCloud >= timerDelayCloud) {
-    lastTimeCloud = millis();
-    
-    ThingSpeak.setField(1, lastTemp);
-    ThingSpeak.setField(2, lastHum);
-    ThingSpeak.setField(3, lastSoil);
-    ThingSpeak.setField(4, pumpState ? 1 : 0); 
-    ThingSpeak.setField(5, fanState ? 1 : 0);
+    lcd.setCursor(0,2);
+    lcd.print("Pompa : ");
+    if (pumpState) lcd.print("ON ");
+    else lcd.print("OFF");
+    lcd.print("      ");
 
-    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-    if(x == 200){
-      Serial.println("Update ThingSpeak berhasil.");
-    }
+    // Menampilkan status sistem di baris ke-4
+    lcd.setCursor(0,3);
+    lcd.print("Kinanthi Maya Oktavi");
+
+    // Output ke Serial Monitor
+    Serial.print("Soil: ");
+    Serial.print(lastSoil);
+    Serial.print("% | Pump: ");
+    Serial.println(pumpState ? "ON" : "OFF");
   }
 }
